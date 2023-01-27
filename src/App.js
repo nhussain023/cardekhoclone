bf6e4402855b9c79f895127e55607ea65fe479bb
Gaurav Sharma <gaurav.sharma4@fisglobal.com>
2023-01-27 17:17:44 +0530

    Car dekho clone implemented

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Headerbar from "./components/header/Headerbar";
+import Footer from "./components/footer/Footer";
+import Slideshow from "./components/banner/Slideshow";
+import SearchedCar from "./components/SearchedCarSection/SearchedCar";
+// import Card from "./components/common/card";
+
+function App() {
+  
+  return (
+    <>
+    <Headerbar/>
+    <Slideshow/>
+    <SearchedCar/>
+    <Footer/>
+   
+    </>
+  );
+}
+
+export default App;
